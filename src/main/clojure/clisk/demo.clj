ef984113c72d7e26a71d86eb7ff12553e308d0f8
mikera <Mike@Mike-VAIO>
2012-05-20 19:31:46 +0800

    Perlin noise updates, new samples

--- src/main/clojure/clisk/demo.clj
+++ src/main/clojure/clisk/demo.clj
@@ -5,12 +5,22 @@
 
 (comment
   
-  (show (vscale 10 (vfrac pos)))
+  (show (vscale 0.1 (vfrac pos)))
   
-  (show (vscale 10 (vdot (vfrac pos) (vfrac pos))))
+  (show (vscale 0.1 (vdot (vfrac pos) (vfrac pos))))
   
   ;; Chess board
   (show (vscale 0.25 (checker 0 1)))
+  
+  ;; Basic perlin noise
+  (show (vscale 0.1 noise))
+  
+  ;; offset of checkers using perlin noise
+  (show 
+    (vscale 0.2 
+            (voffset
+              (v* 7 vnoise) 
+              (checker [1 0 0] [1 1 0]))) 512 512)
   
   
   )
--- src/main/clojure/clisk/patterns.clj
+++ src/main/clojure/clisk/patterns.clj
@@ -1,5 +1,6 @@
 (ns clisk.patterns
-  (:use clisk.functions))
+  (:use clisk.functions)
+  (:import clisk.Perlin))
 
 (defn checker 
   ([a b]
@@ -8,3 +9,41 @@
             (clojure.core/- (clisk.functions/frac y) 0.5))
          a
          b)))
+
+(def perlin-noise 
+  '(clisk.Perlin/noise x y z t))
+
+(def noise
+  perlin-noise)
+
+(def snoise
+  '(clisk.Perlin/snoise x y z t))
+
+(def plasma 
+  (cons 'clojure.core/+
+        (map
+          (fn [i]
+            (let [factor (Math/pow 0.5 (inc i))]
+              `(let [~'x (/ ~'x ~factor)
+                     ~'y (/ ~'y ~factor)
+                     ~'z (/ ~'z ~factor)
+                     ~'t (/ ~'t ~factor)]
+                 (clojure.core/* ~factor ~noise))))
+          (range 6))))
+
+(def offsets-for-vectors [[-120.34 +340.21 -13.67 +56.78]
+                          [+12.301 +70.261 -167.678 +34.568]
+                          [+78.676 -178.678 -79.612 -80.111]
+                          [-78.678 7.6789 200.567 124.099]])
+
+(defn vector-offsets [func]
+  (vec 
+    (map
+      (fn [offs]
+        `(let [~@(interleave pos (map #(do `(clojure.core/+ ~%1 ~%2)) offs pos))] 
+           ~func))
+      offsets-for-vectors)))
+
+(def vnoise (vector-offsets noise))
+
+(def vsnoise (vector-offsets snoise))
