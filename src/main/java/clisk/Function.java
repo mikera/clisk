b39c400b4fd666ca967ccb0ee9baeb3d1c4ab214
mikera <Mike@Mike-VAIO>
2012-05-20 14:29:15 +0800

    Include clisk.Function

---
+++ src/main/java/clisk/Function.java
@@ -0,0 +1,193 @@
+package clisk;
+
+import clojure.lang.ISeq;
+
+public class Function implements clojure.lang.IFn {
+	public double calc() {
+		return calc(0.0);
+	}
+	
+	public double calc(double x) {
+		return calc(x,0.0);
+	}
+	
+	public double calc(double x, double y) {
+		return calc(x,y,0.0);
+	}
+
+	public double calc(double x, double y, double z) {
+		return calc(x,y,z,0.0);
+	}
+	
+	public double calc(double x, double y, double z, double t) {
+		throw new Error("Function not implemented!");
+	}
+	
+	private static double db(Object num) {
+		return ((Number)num).doubleValue();
+	}
+	
+	@Override
+	public Object invoke() {
+		return calc();
+	}
+	
+	@Override
+	public Object invoke(Object x) {
+		return calc(db(x));
+	}
+	
+	@Override
+	public Object invoke(Object x, Object y) {
+		return calc(db(x),db(y));
+	}
+	
+	@Override
+	public Object invoke(Object x, Object y, Object z) {
+		return calc(db(x),db(y),db(z));
+	}
+	
+	@Override
+	public Object invoke(Object x, Object y, Object z, Object t) {
+		return calc(db(x),db(y),db(z),db(t));
+	}
+
+	@Override
+	public Object call() throws Exception {
+		return invoke();
+	}
+
+	@Override
+	public void run() {
+		invoke();
+	}
+
+	@Override
+	public Object invoke(Object arg1, Object arg2, Object arg3, Object arg4,
+			Object arg5) {
+		throw new Error("Unsupported arity!");
+	}
+
+	@Override
+	public Object invoke(Object arg1, Object arg2, Object arg3, Object arg4,
+			Object arg5, Object arg6) {
+		throw new Error("Unsupported arity!");
+	}
+
+	@Override
+	public Object invoke(Object arg1, Object arg2, Object arg3, Object arg4,
+			Object arg5, Object arg6, Object arg7) {
+		throw new Error("Unsupported arity!");
+	}
+
+	@Override
+	public Object invoke(Object arg1, Object arg2, Object arg3, Object arg4,
+			Object arg5, Object arg6, Object arg7, Object arg8) {
+		throw new Error("Unsupported arity!");
+	}
+
+	@Override
+	public Object invoke(Object arg1, Object arg2, Object arg3, Object arg4,
+			Object arg5, Object arg6, Object arg7, Object arg8, Object arg9) {
+		throw new Error("Unsupported arity!");
+	}
+
+	@Override
+	public Object invoke(Object arg1, Object arg2, Object arg3, Object arg4,
+			Object arg5, Object arg6, Object arg7, Object arg8, Object arg9,
+			Object arg10) {
+		throw new Error("Unsupported arity!");
+	}
+
+	@Override
+	public Object invoke(Object arg1, Object arg2, Object arg3, Object arg4,
+			Object arg5, Object arg6, Object arg7, Object arg8, Object arg9,
+			Object arg10, Object arg11) {
+		throw new Error("Unsupported arity!");
+	}
+
+	@Override
+	public Object invoke(Object arg1, Object arg2, Object arg3, Object arg4,
+			Object arg5, Object arg6, Object arg7, Object arg8, Object arg9,
+			Object arg10, Object arg11, Object arg12) {
+		throw new Error("Unsupported arity!");
+	}
+
+	@Override
+	public Object invoke(Object arg1, Object arg2, Object arg3, Object arg4,
+			Object arg5, Object arg6, Object arg7, Object arg8, Object arg9,
+			Object arg10, Object arg11, Object arg12, Object arg13) {
+		throw new Error("Unsupported arity!");
+	}
+
+	@Override
+	public Object invoke(Object arg1, Object arg2, Object arg3, Object arg4,
+			Object arg5, Object arg6, Object arg7, Object arg8, Object arg9,
+			Object arg10, Object arg11, Object arg12, Object arg13, Object arg14) {
+		throw new Error("Unsupported arity!");
+	}
+
+	@Override
+	public Object invoke(Object arg1, Object arg2, Object arg3, Object arg4,
+			Object arg5, Object arg6, Object arg7, Object arg8, Object arg9,
+			Object arg10, Object arg11, Object arg12, Object arg13,
+			Object arg14, Object arg15) {
+		throw new Error("Unsupported arity!");
+	}
+
+	@Override
+	public Object invoke(Object arg1, Object arg2, Object arg3, Object arg4,
+			Object arg5, Object arg6, Object arg7, Object arg8, Object arg9,
+			Object arg10, Object arg11, Object arg12, Object arg13,
+			Object arg14, Object arg15, Object arg16) {
+		throw new Error("Unsupported arity!");
+	}
+
+	@Override
+	public Object invoke(Object arg1, Object arg2, Object arg3, Object arg4,
+			Object arg5, Object arg6, Object arg7, Object arg8, Object arg9,
+			Object arg10, Object arg11, Object arg12, Object arg13,
+			Object arg14, Object arg15, Object arg16, Object arg17) {
+		throw new Error("Unsupported arity!");
+	}
+
+	@Override
+	public Object invoke(Object arg1, Object arg2, Object arg3, Object arg4,
+			Object arg5, Object arg6, Object arg7, Object arg8, Object arg9,
+			Object arg10, Object arg11, Object arg12, Object arg13,
+			Object arg14, Object arg15, Object arg16, Object arg17, Object arg18) {
+		throw new Error("Unsupported arity!");
+	}
+
+	@Override
+	public Object invoke(Object arg1, Object arg2, Object arg3, Object arg4,
+			Object arg5, Object arg6, Object arg7, Object arg8, Object arg9,
+			Object arg10, Object arg11, Object arg12, Object arg13,
+			Object arg14, Object arg15, Object arg16, Object arg17,
+			Object arg18, Object arg19) {
+		throw new Error("Unsupported arity!");
+	}
+
+	@Override
+	public Object invoke(Object arg1, Object arg2, Object arg3, Object arg4,
+			Object arg5, Object arg6, Object arg7, Object arg8, Object arg9,
+			Object arg10, Object arg11, Object arg12, Object arg13,
+			Object arg14, Object arg15, Object arg16, Object arg17,
+			Object arg18, Object arg19, Object arg20) {
+		throw new Error("Unsupported arity!");
+	}
+
+	@Override
+	public Object invoke(Object arg1, Object arg2, Object arg3, Object arg4,
+			Object arg5, Object arg6, Object arg7, Object arg8, Object arg9,
+			Object arg10, Object arg11, Object arg12, Object arg13,
+			Object arg14, Object arg15, Object arg16, Object arg17,
+			Object arg18, Object arg19, Object arg20, Object... args) {
+		throw new Error("Unsupported arity!");
+	}
+
+	@Override
+	public Object applyTo(ISeq arglist) {
+		throw new Error("Unsupported arity!");
+	}
+}
