43e4698f42de6e6d0afc82a8e9b1db1a54069e83
mikera <Mike@Mike-VAIO>
2012-05-20 15:41:56 +0800

    Include new files

---
+++ src/test/clojure/clisk/test_functions.clj
@@ -0,0 +1,8 @@
+(ns clisk.test-functions
+  (:use clojure.test)
+  (:use clisk.functions)
+  (:import clisk.Util))
+
+(deftest test-plus
+  (testing "Colours"
+    (is (= [1.0 2.0] (map eval (v+ [1.0 1.0] [0.0 1.0]))))))
