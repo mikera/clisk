caf6ac36d5e84117eba07ca3137b32bbf8968b34
mikera <mike.r.anderson.13@gmail.com>
2012-05-20 14:28:55 +0800

    Basic image generation

--- src/main/clojure/clisk/core.clj
+++ src/main/clojure/clisk/core.clj
@@ -1,4 +1,14 @@
-(ns clisk.core)
+(ns clisk.core
+  (:import clisk.Util))
+
+(set! *warn-on-reflection* true)
+
+(def DEFAULT-IMAGE-WIDTH 256)
+
+(def DEFAULT-IMAGE-HEIGHT 256)
+
+(defn error [& vals]
+  (throw (Error. (str (reduce str vals)))))
 
 (defn vectorize-item [code]
   (cond 
@@ -25,3 +35,85 @@
 
 (defmacro vectorize [dims code]
   (vectorize* dims code))
+
+
+(defn check-dims [& vectors]
+  (if (every? vector? vectors)
+    (let [[v & vs] vectors
+          dims (count v)]
+      (if (every? #(= dims (count %)) vs)
+        dims
+        (error "Unequal vector sizes: " (map count vectors))))
+    (error "Not all vectors: " vectors)))
+
+(defn vectorize-op1 [f]
+  (fn [v1]
+    (let [v1 v1
+         dims (check-dims v1)]
+      (vec (for [i (range dims)]
+           (list f (v1 i)))))))
+
+(defn vectorize-op2 [f]
+  (fn [v1 v2]
+    (let [v1 v1
+          v2 v2
+          dims (check-dims v1 v2)]
+      (vec (for [i (range dims)]
+           (list f (v1 i) (v2 i)))))))
+
+(def vsin
+  (vectorize-op1 'Math/sin))
+
+(def v+ 
+  (vectorize-op2 'clojure.core/+))
+
+(def v* 
+  (vectorize-op2 'clojure.core/*))
+
+(def v- 
+  (vectorize-op2 'clojure.core/-))
+
+(def vdivide 
+  (vectorize-op2 'clojure.core//))
+  
+(defn ^clisk.Function compile-fn [code]
+  (eval
+    `(proxy [clisk.Function] []
+       (calc 
+         ([~'x ~'y ~'z ~'t]
+           (double ~code))
+         ([~'x ~'y ~'z]
+           (.calc ~'this ~'x ~'y ~'z 0.0))
+         ([~'x ~'y]
+           (.calc ~'this ~'x ~'y 0.0))
+         ([~'x]
+           (.calc ~'this ~'x 0.0))
+         ([]
+           (.calc ~'this 0.0))))))
+
+(defn img
+  ([vector-function]
+    (img vector-function DEFAULT-IMAGE-WIDTH DEFAULT-IMAGE-HEIGHT))
+  ([vector-function w h]
+    (img vector-function w h 1.0 (/ (double h) (double w))))
+  ([vector-function w h dx dy]
+    (let [image (Util/newImage (int w) (int h))
+          fr (compile-fn (vector-function 0))
+          fg (compile-fn (vector-function 1))
+          fb (compile-fn (vector-function 2))]
+	    (dotimes [iy h] 
+	      (dotimes [ix w]
+	        (let [x (/ (* dx ix) (double w))
+                y (/ (* dy iy) (double h))
+                r (fr x y)
+                g (fg x y)
+                b (fb x y)
+                argb (Util/toARGB r g b)]
+           (.setRGB image ix iy argb))))
+     image)))
+
+(defn show 
+  ([vector-function]
+    (show vector-function DEFAULT-IMAGE-WIDTH DEFAULT-IMAGE-HEIGHT))
+  ([vector-function w h]
+    (Util/show (img vector-function w h))))
--- src/test/clojure/clisk/test_core.clj
+++ src/test/clojure/clisk/test_core.clj
@@ -1,3 +1,14 @@
 (ns clisk.test-core
-  (:use clojure.test))
+  (:use clojure.test)
+  (:use clisk.core)
+  (:import clisk.Util))
 
+(deftest test-colour
+  (testing "Colours"
+    (is (= (unchecked-int 0xFF000000) (Util/toARGB 0.0 0.0 0.0)))
+    (is (= (unchecked-int 0xFFFF0000) (Util/toARGB 1.0 0.0 0.0)))
+    (is (= (unchecked-int 0xFFFFFFFF) (Util/toARGB 1.0 1.0 1.0)))))
+
+(deftest test-compile
+  (testing "Colours"
+    (is (= 1.0 ((compile-fn 1.0) 0)))))
