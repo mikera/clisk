38a8ad1defb63f9e4e1e4b7298c61c98a6221041
mikera <Mike@Mike-VAIO>
2012-05-20 15:41:30 +0800

    Basic function implementations

--- src/main/clojure/clisk/core.clj
+++ src/main/clojure/clisk/core.clj
@@ -7,74 +7,12 @@
 
 (def DEFAULT-IMAGE-HEIGHT 256)
 
-(defn error [& vals]
-  (throw (Error. (str (reduce str vals)))))
-
-(defn vectorize-item [code]
-  (cond 
-    (number? code) (double code)
-    (symbol? code) code
-    (sequential? code) code
-    :else (throw (Error. (str "Invalid vector item: " (type code))))))
-
-(defn vectorize* 
-  ([dims code]
-	  (cond
-	    (vector? code) (let [c (count code)]
-	                  (if (< c dims)
-	                    (vec (concat code (repeat (- dims c) 0.0)))
-	                    code))
-	    (sequential? code)
-	      (let [[op & more] code
-              more (map vectorize-item more)
-	            vectorized-more (map #(vectorize* dims %) more)]
-	        (vec (for [i (range dims)]
-	          (cons op (map #(% i) vectorized-more)))))
-	    :else 
-       (vec (repeat dims (vectorize-item code))))))
-
-(defmacro vectorize [dims code]
-  (vectorize* dims code))
 
 
-(defn check-dims [& vectors]
-  (if (every? vector? vectors)
-    (let [[v & vs] vectors
-          dims (count v)]
-      (if (every? #(= dims (count %)) vs)
-        dims
-        (error "Unequal vector sizes: " (map count vectors))))
-    (error "Not all vectors: " vectors)))
 
-(defn vectorize-op1 [f]
-  (fn [v1]
-    (let [v1 v1
-         dims (check-dims v1)]
-      (vec (for [i (range dims)]
-           (list f (v1 i)))))))
 
-(defn vectorize-op2 [f]
-  (fn [v1 v2]
-    (let [v1 v1
-          v2 v2
-          dims (check-dims v1 v2)]
-      (vec (for [i (range dims)]
-           (list f (v1 i) (v2 i)))))))
 
-(def vsin
-  (vectorize-op1 'Math/sin))
 
-(def v+ 
-  (vectorize-op2 'clojure.core/+))
-
-(def v* 
-  (vectorize-op2 'clojure.core/*))
-
-(def v- 
-  (vectorize-op2 'clojure.core/-))
-
-(def vdivide 
-  (vectorize-op2 'clojure.core//))
   
 (defn ^clisk.Function compile-fn [code]
   (eval
