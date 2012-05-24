23d08d4282db49e4917b711913c13f419519cfb8
mikera <mike.r.anderson.13@gmail.com>
2012-05-24 12:40:59 +0800

    Java side generator functions

---
+++ src/test/clojure/clisk/test_generator.clj
@@ -0,0 +1,12 @@
+(ns clisk.test-generator
+  (:use clojure.test)
+  (:use clisk.core)
+  (:import clisk.Generator)
+  (:import java.awt.image.BufferedImage))
+
+(deftest test-gen
+  (testing "Basic generator"
+    (let [^BufferedImage b (Generator/generate "[1 1 1]" 10 12)]
+      (is (== 10 (.getWidth b)))
+      (is (== 12 (.getHeight b)))
+      (is (== (unchecked-int 0xFFFFFFFF) (.getRGB b 0 0))))))
