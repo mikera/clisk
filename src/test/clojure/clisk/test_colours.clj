39a149b51beef60e4f81207add05ac09828a7fae
mikera <Mike@Mike-VAIO>
2012-05-24 14:39:52 +0800

    Java colours

---
+++ src/test/clojure/clisk/test_colours.clj
@@ -0,0 +1,8 @@
+(ns clisk.test-colours
+  (:use clojure.test)
+  (:use clisk.colours))
+
+(deftest test-basic-colours
+  (testing "Java colours"
+    (is (= [1.0 1.0 1.0 1.0] white))
+    (is (= [0.0 0.0 0.0 1.0] black))))
