45dec682fd1061a142f813614ccd72a8295bf44e
mikera <Mike@10.1.1.107>
2012-06-29 21:21:08 +0800

    Interim AST-node work

--- src/main/clojure/clisk/node.clj
+++ src/main/clojure/clisk/node.clj
@@ -5,32 +5,26 @@
 
 (declare node)
 
+(defn new-node [props]
+  (Node. nil props))
+
+
 (defn node? [x] 
   (instance? Node x))
 
-(defn constant? [x] 
+(defn constant-node? [x] 
   (and (node? x) (:constant x)))
-
-(defn new-node [props]
-  (Node. nil props))
 
 (defn constant-node [v]
   (cond
     (vector? v)
       (new-node {:type :vector
-             :codes (vec (map double v))
+             :nodes (vec (map double v))
              :constant true})
     :else 
       (new-node {:type :scalar 
              :code (double v)
              :constant true})))
-
-(defn validate [node]
-  (cond
-    (not (xor (:code node) (:codes node))) 
-      (error "AST node must have :code or :codes")
-    :else 
-      node))
 
 (defn vec-node [[xs]]
   (let [nodes (map node xs)]
@@ -40,13 +34,30 @@
       :else
         (new-node 
           {:type :vector
+           :nodes (vec nodes)
            :codes (vec (map :code nodes))
-           :constant (every? constant? nodes)}))))
+           :constant (every? constant-node? nodes)}))))
 
 (defn node [x]
-  "Creates a node from arbitrary input"
+  "Creates a node from arbitrary input. Idempotent, can be used to force conversion to node."
   (cond 
     (node? x) x
     (number? x) (constant-node x)
-    (sequential? x) (vec-node x)
+    (vector? x) (vec-node x)
     :else (error "Unable to build an AST node from: " x)))
+
+(def ZERO-NODE (node 0.0))
+
+(defn component [i n]
+  "Returns a node that represents the specified component of an input node"
+  (let [n (node n)]
+	  (if (= :vector (:type n))
+	    (nth (:nodes node) i ZERO-NODE)
+	    node)))
+
+(defn validate [node]
+  (cond
+    (not (xor (:code node) (:nodes node))) 
+      (error "AST node must have :code or :nodes")
+    :else 
+      node))
