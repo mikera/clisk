f10615470c00b31cf0cd9a5fb1c5b248ca9e66b0
mikera <mike.r.anderson.13@gmail.com>
2012-07-17 00:35:32 +0800

    Constant compilation

--- src/main/clojure/clisk/node.clj
+++ src/main/clojure/clisk/node.clj
@@ -4,12 +4,61 @@
 (defrecord Node [])
 
 (declare node)
+(declare constant-node)
+(declare evaluate)
 
 ;; standard position vector
 (def pos ['x 'y 'z 't])
 
-(defn new-node [props]
-  (Node. nil props))
+(defn value-node [num]
+  (Node. nil
+         {:type :scalar 
+          :code (double num)
+          :constant true}))
+
+(defn new-node 
+  "Create a new AST node with the given properties"
+  ([props]
+	  (let [n (Node. nil props)]
+		  (if (and (:constant props) (not (number? (:code props))))
+		    (value-node (evaluate n))
+		    n))))
+
+
+(defn compile-scalar-node [n]
+  "Compile a scalar node to a clisk.IFunction"
+  (let [n (node n)
+        obj-map (:objects n)
+        syms (keys obj-map)
+        objs (vals obj-map)
+        code (:code n)]
+    (apply
+      (eval
+		   `(fn [~@syms]
+	        (let []
+		        (reify clisk.IFunction
+			       (calc 
+			         [~'this ~'x ~'y ~'z ~'t]
+			           (double ~code))
+			       (calc
+			         [~'this ~'x ~'y ~'z]
+			           (.calc ~'this ~'x ~'y ~'z 0.0))
+			       (calc
+			         [~'this ~'x ~'y]
+			           (.calc ~'this ~'x ~'y 0.0))
+			       (calc
+			         [~'this ~'x]
+			           (.calc ~'this ~'x 0.0))
+			       (calc
+			         [~'this]
+			           (.calc ~'this 0.0))))))
+	      objs)))
+
+(defn evaluate 
+  "Evaluates a node at the zero position"
+  ([n]
+    (let [n (node n)]
+      (.calc (compile-scalar-node n)))))
 
 (defn node? [x] 
   (instance? Node x))
@@ -33,10 +82,38 @@
           {:type :vector
            :nodes (vec nodes)
            :codes (vec (map :code nodes))
+           :objects (apply merge (map :objects nodes))
            :constant (every? constant-node? nodes)}))))
 
 (defn vector-node [& xs] 
   (vec-node xs))
+
+(defn object-node [v]
+  (let [sym (gensym "obj")]
+    (new-node 
+          {:type :scalar
+           :code sym
+           :objects {sym v}
+           :constant true})))
+
+(defn transform-node
+  "Creates a node containing code that trasnforms the other scalar vectors into a new code form"
+  ([f & scalars]
+    (let [scalars (map node scalars)]
+      (if-not (every? scalar-node? scalars) (error "Nodes to transform must be scalar"))
+	    (new-node
+	      {:type :scalar
+	       :code (apply f (map :code scalars))
+	       :objects (apply merge (map :objects scalars))
+	       :constant (every? constant-node? scalars)}))))
+
+(defn function-node
+  "Creates a node which is a function of scalar nodes"
+  ([f & scalars]
+    (apply 
+      transform-node
+      (fn [& xs] `(~f ~@xs))
+      scalars)))
 
 (defn constant-node [v]
   (cond
@@ -45,13 +122,12 @@
         (if (not (:constant node)) (error "Not a constant vector!"))
         node) 
     :else 
-      (new-node {:type :scalar 
-             :code (double v)
-             :constant true})))
+      (value-node (double v))))
 
 (defn code-node [form]
   (new-node {:type :scalar 
              :code form
+             :objects (apply merge (map :objects (filter node? (flatten form))))
              :constant false}))
 
 (defn node [a]
@@ -63,6 +139,7 @@
     (fn? a) (node (a pos))
     (symbol? a) (code-node a)
     (sequential? a) (code-node a)
+    :object (object-node a)
     :else (error "Unable to build an AST node from: " a)))
 
 (def ZERO-NODE (node 0.0))
@@ -74,13 +151,15 @@
 	    (nth (:nodes n) i ZERO-NODE)
 	    n))) 
 
-(defn dimensions [x]
-  (let [x (node x)]
-    (cond
-      (scalar-node? x)
-        1
-      (vector-node? x)
-        (count (:nodes x)))))
+(defn dimensions 
+  "Returns the number of dimensions in a vector node, or 1 if scalar"
+  ([x]
+	  (let [x (node x)]
+	    (cond
+	      (scalar-node? x)
+	        1
+	      (vector-node? x)
+	        (count (:nodes x))))))
 
 (defn validate [node]
   (cond
