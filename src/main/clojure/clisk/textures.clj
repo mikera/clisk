5fbf7cc5767a21b1a5f9b1d79955ec9ea5c55bb0
mikera <mike.r.anderson.13@gmail.com>
2012-07-27 16:14:26 +0800

    Extra texture functions

--- src/main/clojure/clisk/textures.clj
+++ src/main/clojure/clisk/textures.clj
@@ -3,7 +3,12 @@
 
 (def agate
   "A monochrome agate-style rock texture"
-    (vscale 0.3 
-			(voffset 
-			  (v* 4 plasma)
-			  (colour-map [[0 0] [0.05 0.5] [0.5 0.9] [0.95 0.5] [1.0 0]] vfrac))))
+  (vscale 0.3 
+		(voffset 
+		  (v* 4 plasma)
+		  (colour-map [[0 0] [0.05 0.5] [0.5 0.9] [0.95 0.5] [1.0 0]] vfrac))))
+
+(def clouds
+  "A cloudlike texture"
+  (vscale 0.3
+     (v- 1 (vpow plasma 3)) ))
