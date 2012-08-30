61ca13ac8d5bdeb3bf4ea156f18ddee918e8b278
mikera <mike.r.anderson.13@gmail.com>
2012-08-30 17:53:28 +0800

    Mandelbrot fractal to celebrate 0.4.2 release

--- src/main/clojure/clisk/samples/gallery.clj
+++ src/main/clojure/clisk/samples/gallery.clj
@@ -52,6 +52,16 @@
   
   :colourful-spots
   (show (seamless 0.5 (compose vnoise [spots y z t])) 1024 1024)
+  
+  :mandelbrot
+  (show (viewport [-2 -1.5] [1 1.5]
+          (fractal 
+            :init pos
+            :while (v- 2 (length [x y])) 
+            :update (v+ c [(v- (v* x x) (v* y y))  (v* 2 x y)]) 
+            :result (vplasma (v* 0.1 'i))
+            :bailout-result black
+            :max-iterations 1000)) 1024 1024)
     
 })
 
