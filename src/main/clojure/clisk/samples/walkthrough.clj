a54bb4ce94e6b0f12d222305297a783287490907
mikera <mike.r.anderson.13@gmail.com>
2012-09-07 13:35:59 +0800

    Added walkthorugh code

---
+++ src/main/clojure/clisk/samples/walkthrough.clj
@@ -0,0 +1,18 @@
+(ns clisk.samples.walkthrough
+  (:use [clisk core node functions patterns colours textures util]))
+
+(defn walthrough [] 
+  
+;; colours in RGB  
+(show [1 0 0])
+
+;; named colours
+(show pink)
+
+
+(show [x 0 0])
+
+(show [x y 0])
+
+)
+
