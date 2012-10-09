c8d2f397a7c1aabac9d31ae8db486a520a7006af
mikera <mike.r.anderson.13@gmail.com>
2012-10-09 09:03:43 +0800

    Add vectorfunction constructor

--- src/main/java/clisk/VectorFunction.java
+++ src/main/java/clisk/VectorFunction.java
@@ -1,11 +1,24 @@
 package clisk;
 
+import java.util.List;
+
 import mikera.vectorz.AVector;
 
 public class VectorFunction extends mikera.vectorz.functions.VectorFunction {
-	private int inputDimensions;
-	private int outputDimensions;
-	private Function[] functions;
+	private final int inputDimensions;
+	private final int outputDimensions;
+	private final Function[] functions;
+	
+	private VectorFunction(int inputs, int outputs, Function[] functions) {
+		this.inputDimensions=inputs;
+		this.outputDimensions=outputs;
+		this.functions=functions;
+	}
+	
+	public VectorFunction create(int inputDims, List<Function> functions) {
+		Function[] funcs=functions.toArray(new Function[functions.size()]);
+		return new VectorFunction(inputDims,funcs.length,funcs);
+	}
 	
 	@Override
 	public void transform(AVector source, AVector dest) {
@@ -24,5 +37,4 @@ public class VectorFunction extends mikera.vectorz.functions.VectorFunction {
 	public int outputDimensions() {
 		return outputDimensions;
 	}
-
 }
