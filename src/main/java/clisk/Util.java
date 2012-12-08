6cbadea907c88550c8987746430aa06c5b57a655
mikera <mike.r.anderson.13@gmail.com>
2012-12-08 11:39:57 +0800

    Tidy imports

--- src/main/java/clisk/Util.java
+++ src/main/java/clisk/Util.java
@@ -4,8 +4,6 @@ import java.awt.Dimension;
 
 import java.awt.FileDialog;
 import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.RenderingHints;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
