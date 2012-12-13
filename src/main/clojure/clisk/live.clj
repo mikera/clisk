3da053c0b82436ab90f766d19a94f05cc7337a9f
mikera <mike.r.anderson.13@gmail.com>
2012-12-13 16:40:24 +0800

    clisk.live namespace edits

--- src/main/clojure/clisk/live.clj
+++ src/main/clojure/clisk/live.clj
@@ -2,4 +2,11 @@
   ^{:author "mikera"
     :doc "Namespace with all includes ready for live coding."}    
   clisk.live
+;;  (:refer-clojure :exclude [* + - /])
   (:use [clisk core node functions patterns colours textures util effects]))
+
+;; do we really want to override these???
+;;(def * v*)
+;;(def + v+)
+;;(def - v-)
+;;(def / clisk.functions/vdivide)
