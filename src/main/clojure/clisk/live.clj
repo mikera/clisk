94ff63d85c2c3b47ab493e3a065b9a9e9887c040
mikera <mike.r.anderson.13@gmail.com>
2012-12-13 15:19:34 +0800

    Add clisk.live namespace

---
+++ src/main/clojure/clisk/live.clj
@@ -0,0 +1,5 @@
+(ns 
+  ^{:author "mikera"
+    :doc "Namespace with all includes ready for live coding."}    
+  clisk.live
+  (:use [clisk core node functions patterns colours textures util effects]))
