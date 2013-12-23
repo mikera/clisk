4e00b3eb861f4bc16982ebfae63c87154163f512
mikera <mike.r.anderson.13@gmail.com>
2013-12-23 13:47:54 +0000

    update core to use imagez

--- src/main/clojure/clisk/core.clj
+++ src/main/clojure/clisk/core.clj
@@ -3,10 +3,11 @@
     :doc "Core clisk image generation functions"}
   clisk.core
   (:import clisk.Util)
-  (:import java.awt.image.BufferedImage)
+  (:import [java.awt.image BufferedImage])
   (:import [mikera.gui Frames])
   (:import [javax.swing JComponent])
   (:require [clojure test])
+  (:require [mikera.image.core :as imagez])
   (:use [clisk node functions util]))
 
 (set! *warn-on-reflection* true)
@@ -29,9 +30,10 @@
 
 (defn tst [] (clojure.test/run-all-tests))
 
-(defn scale-image [img w h]
+(defn scale-image 
   "Scales an image to a given width and height"
-  (Util/scaleImage img (int w) (int h)))
+  (^BufferedImage [^BufferedImage img w h]
+    (imagez/scale-image img w h)))
 
 (defn show-comp 
   "Shows a component in a new frame"
@@ -50,6 +52,20 @@
           input-dimensions (int (or input-dimensions 4))
           ^java.util.List funcs (vec (map compile-fn (:nodes a)))]
       (clisk.VectorFunction/create input-dimensions funcs))))
+
+(defn image
+  "Creates a bufferedimage from the given clisk data"
+  ([vector-function
+    & {:keys [width height size anti-alias] 
+       :or {size DEFAULT-IMAGE-SIZE}}]
+    (let [vector-function (validate (node vector-function))
+          scale (or anti-alias *anti-alias*)
+          w (int (or width size))
+          h (int (or height size))
+          fw (* w scale)
+          fh (* h scale)
+          img (img vector-function fw fh)]
+      (scale-image img w h))))
 
 (defn show 
   "Creates and shows an image from the given vector function"
