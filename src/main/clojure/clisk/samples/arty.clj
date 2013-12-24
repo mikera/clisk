06e3f673f872df28292c0405bf85766020763d76
mikera <mike.r.anderson.13@gmail.com>
2013-12-24 21:36:29 +0000

    pastels and strings

--- src/main/clojure/clisk/samples/arty.clj
+++ src/main/clojure/clisk/samples/arty.clj
@@ -8,8 +8,11 @@
   ;; 2: Christmas Spots
   (show (seamless 0.5 (v* [1 0.8 0 0] (compose vnoise [spots y z t]))) :size 512)
   
+  ;; 3. String Theory
+  (show (seamless 0.2 (v* 10 (scale 3 vnoise) (v- 0.1 (vmax 0 (vabs (v- plasma 0.5)))))) :size 512)
   
-  
+  ;; 4. Pastels
+  (show (seamless 1.0 (offset (v* 4 vplasma) (v+ (offset 10 vnoise) 0.3))) :size 512)
   
   
   (show (let [voronoi1 (voronoi :points 100)] 
