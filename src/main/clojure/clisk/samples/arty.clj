876433417f03c8fcdb892a08b0472acc03df2d7c
mikera <mike.r.anderson.13@gmail.com>
2013-12-24 19:56:43 +0000

    pattern 1 grey noise with colour

--- src/main/clojure/clisk/samples/arty.clj
+++ src/main/clojure/clisk/samples/arty.clj
@@ -2,7 +2,17 @@
   (:use [clisk live]))
 
 (defn demo []
-  ;; patterned grey noise
-  (show (offset (v* 0.1 (scale 0.03 vsnoise)) (rgb-from-hsl (v+ [0 0 0.5] (scale 0.3 snoise)))) :size 512)
-    
+  ;; patterned grey noise with rainbow patches
+  (show (offset (v* 0.1 (scale 0.03 vsnoise)) (rgb-from-hsl (v+ [0 0 0.5] (scale 0.3 vsnoise)))) :size 512)
+  
+  (show (let [voronoi1 (voronoi :points 100)] 
+    (v* 20.0 (voronoi-blocks :voronoi voronoi1)))
+    :size 256)  
+  
+  (show (vmax 0 (v+ -1.3 (scale 0.1 (v+ (vsin x) (vcos y))))))
+  
+  (show (rgb-from-hsl vsplasma))
+  
+  ;; islands
+  (show (scale 0.25 (desert-map plasma)))
   )
