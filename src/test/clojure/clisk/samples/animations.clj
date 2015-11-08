5e41e8a096f2cc1adaf6a5b38f389736b9335b33
mikera <mike.r.anderson.13@gmail.com>
2015-11-08 22:43:32 +0800

    Add animation with 480P

--- src/test/clojure/clisk/samples/animations.clj
+++ src/test/clojure/clisk/samples/animations.clj
@@ -1,8 +1,19 @@
 (ns clisk.samples.animations
-  (:use [clisk live]))
+  (:use [clisk live])
+  (:import [clisk Util]))
 
-(defn demo []
-  ;; we do this to avoid failure in testing if telegenic is not present
+(comment
+  ;; we do this to avoid failure in testing if jcodec-javase is not present
   (require '[telegenic.core :as telegenic])
   
+  (telegenic/encode
+    (map 
+      (fn [i]
+        (let [im (image (offset [0 0 (* i 0.003)]   
+                                (scale 0.5 (clisk.live/vabs (clisk.live/length (clisk.live/vsin (clisk.live/v* (clisk.live/lightness-from-rgb (clisk.live/dot [-0.7247 -0.5677] (clisk.live/v+ [-2.4375 -1.7593 -1.0106] [0.7885 -1.7012 1.7523 0.2914]))) (clisk.live/vdivide (clisk.live/saturation-from-rgb (clisk.live/lerp (clisk.live/adjust-hsl (clisk.live/adjust-hsl clisk.live/vsnoise [0.795 2.244 -0.63]) [-1.9613 -1.3866 -0.0114 1.3613]) (clisk.live/y (clisk.live/v* clisk.live/pos 1.2235)) [-1.4063 3.9377999999999997 -1.4972])) (clisk.live/lightness-from-rgb (clisk.live/y (clisk.live/alpha [-2.4185 -1.3271])))))))))  )
+                        :width 854 :height 480)]
+          (Util/show im)
+          im))
+      (range 300))
+    {:filename "out.mp4"})
   )
