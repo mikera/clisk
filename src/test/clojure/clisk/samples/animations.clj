11df90ed98470bcb87dea9bd935fd6062b933cb8
mikera <mike.r.anderson.13@gmail.com>
2015-11-11 09:50:12 +0800

    refactor all animations to use render-animation macro

--- src/test/clojure/clisk/samples/animations.clj
+++ src/test/clojure/clisk/samples/animations.clj
@@ -27,6 +27,16 @@
   ;; we do this to avoid failure in testing if jcodec-javase is not present
   (require '[telegenic.core :as telegenic])
   
+  
+  ;; Simple example - scrolling in x axis
+  (render-animation [i 50]
+    (offset [(* i 0.04) 0]     ;; use an offset in x-axis to scroll the image over time  
+            (vnoise vsnoise))  ;; use any Clisk function to create the image
+    {:filename "out.mp4" ;; filename to output 
+     :width 256          ;; width of generated video
+     :height 256})       ;; height of generated video
+  
+  
   ;; Clojure procedural animation #1 - https://www.youtube.com/watch?v=XKV209VfkTk
   ;; black and white swirly patterns
   (render-animation [i 300]
@@ -39,11 +49,11 @@
   ;; several hours to render!
   ;; 151104_023134_N.clj
   (render-animation [i 600]
-        (offset [0 0 (* i 0.003)] ;; use an offset in z-axis to change the image over time     
-                (clisk.live/vabs 
-                  (clisk.live/lerp 
-                    (clisk.live/adjust-hsl (clisk.live/adjust-hsl clisk.live/vsnoise [0.795 2.244 -0.63]) [-1.9613 -1.3866 -0.0114 1.3613]) 
-                    (clisk.live/y (clisk.live/v* clisk.live/pos 1.2235)) [-1.4063 3.9377999999999997 -1.4972])))
+    (offset [0 0 (* i 0.003)] ;; use an offset in z-axis to change the image over time     
+      (clisk.live/vabs 
+        (clisk.live/lerp 
+          (clisk.live/adjust-hsl (clisk.live/adjust-hsl clisk.live/vsnoise [0.795 2.244 -0.63]) [-1.9613 -1.3866 -0.0114 1.3613]) 
+          (clisk.live/y (clisk.live/v* clisk.live/pos 1.2235)) [-1.4063 3.9377999999999997 -1.4972])))
     {})
 
    
@@ -51,55 +61,35 @@
   ;; cloudy skyscape
   ;; several hours to render!
   ;; 150829_223119_C.clj
-  (telegenic/encode
-    (map 
-      (fn [i]
-        (let [im (image (offset [0 0 (* i 0.003)]   
-                                (clisk.live/vpow (clisk.live/length (clisk.live/green-from-hsl (clisk.live/rgb-from-hsl (clisk.live/v- clisk.live/pos (clisk.live/blue-from-hsl (clisk.live/rgb-from-hsl (clisk.live/v- (clisk.live/v+ [0.3541 -1.1358] clisk.live/pos) (clisk.live/vsin (clisk.live/vsin clisk.live/vsnoise))))))))) (clisk.live/vpow (clisk.live/length clisk.live/splasma) (clisk.live/sigmoid (clisk.live/gradient (clisk.live/green-from-hsl (clisk.live/rgb-from-hsl (clisk.live/v- clisk.live/vsnoise (clisk.live/vsin (clisk.live/y clisk.live/vsnoise))))))))))
-                        :width 854 :height 480)]
-          (show im)
-          im))
-      (range 400))
-    {:filename "out.mp4"})
+  (render-animation [i 400]
+    (offset [0 0 (* i 0.003)] ;; use an offset in z-axis to change the image over time  
+      (clisk.live/vpow 
+        (clisk.live/length (clisk.live/green-from-hsl (clisk.live/rgb-from-hsl (clisk.live/v- clisk.live/pos (clisk.live/blue-from-hsl (clisk.live/rgb-from-hsl (clisk.live/v- (clisk.live/v+ [0.3541 -1.1358] clisk.live/pos) (clisk.live/vsin (clisk.live/vsin clisk.live/vsnoise))))))))) 
+        (clisk.live/vpow (clisk.live/length clisk.live/splasma) (clisk.live/sigmoid (clisk.live/gradient (clisk.live/green-from-hsl (clisk.live/rgb-from-hsl (clisk.live/v- clisk.live/vsnoise (clisk.live/vsin (clisk.live/y clisk.live/vsnoise))))))))))
+    {})
+  
   
   ;; Clojure procedural animation #3 - https://www.youtube.com/watch?v=rPEZzUWlDEQ
   ;; colourful glow effects
-  ;; this one also has a [x y] drift to add some variety
-  (telegenic/encode
-       (map 
-         (fn [i]
-           (let [im (image (offset [(* i 0.005) (* i 0.005) (* i 0.01)]   
-                                   (clisk.live/vabs (clisk.live/gradient (clisk.live/green-from-hsl (clisk.live/hue-from-rgb clisk.live/vsnoise)))))
-                           :width 854 :height 480)]
-             (show im)
-             im))
-         (range 400))
-       {:filename "out.mp4"})
+  (render-animation [i 400]
+    (offset [(* i 0.005) (* i 0.005) (* i 0.01)] ;; include [x y] drift to add some scrolling  
+      (clisk.live/vabs (clisk.live/gradient (clisk.live/green-from-hsl (clisk.live/hue-from-rgb clisk.live/vsnoise)))))
+    {})
+  
   
   ;; Clojure procedural animation #4
   ;; Rainbows with checkered discontinuities
-  (telegenic/encode
-       (map 
-         (fn [i]
-           (let [im (image (offset [(* i 0.005) (* i 0.005) (* i 0.01)]   
+  (render-animation [i 400]
+    (offset [(* i 0.005) (* i 0.005) (* i 0.01)]   
                                    (clisk.live/vfrac (clisk.live/vcos (clisk.live/gradient (clisk.live/green-from-hsl (clisk.live/hue-from-rgb clisk.live/vsnoise) )))))
-                           :width 854 :height 480)]
-             (show im)
-             im))
-         (range 400))
-       {:filename "out.mp4"})
+    {})
+  
   
   ;; Clojure procedural animation #5
   ;; Dedicated to Cindy
-  (telegenic/encode
-       (map 
-         (fn [i]
-           (let [im (image (offset [0 0 (* i 0.003)]   
-                                   (scale 0.3 (vplasma vsnoise)))
-                           :width 854 :height 480)]
-             (show im)
-             im))
-         (range 400))
-       {:filename "out.mp4"})
+  (render-animation [i 400]
+    (offset [0 0 (* i 0.003)]   
+      (scale 0.3 (vplasma vsnoise)))
+    {})
   
   )
