bab8c0a0479442f53ff4759c95eef5397e12081d
mikera <mike.r.anderson.13@gmail.com>
2016-01-31 22:53:42 +0800

    Change default scale to 2.0

--- src/main/clojure/clisk/core.clj
+++ src/main/clojure/clisk/core.clj
@@ -84,7 +84,7 @@
       (scale-image img w h))))
 
 ;; scale factor 
-(def ^:dynamic DISPLAY-SCALE 3.0)
+(def ^:dynamic DISPLAY-SCALE 2.0)
 
 (defn show 
   "Creates and shows an image from the given image vector function"
