3a867495162f26f7cec6c7b2241f8907328e7975
mikera <mike.r.anderson.13@gmail.com>
2016-03-19 13:21:48 +0800

    Workshop examples

--- src/test/clojure/clisk/workshop/demo.clj
+++ src/test/clojure/clisk/workshop/demo.clj
@@ -1,3 +1,56 @@
 (ns clisk.workshop.demo
   (:use [clisk live]))
 
+
+;; Image composition example
+(comment 
+  (show (scale 0.05 (vsin x)))
+  
+  (show (scale 0.5 (rgb-from-hsl vnoise)))
+  
+  (show (v* (scale 0.05 (vsin x)) (scale 0.5 (rgb-from-hsl vnoise))))
+  )
+
+;; Walkthrough
+(comment 
+  ;; red along x-axis
+  (show [x 0 0])
+  
+  ;; red along x-asis, green along y-axis
+  (show [x y 0])
+  
+  ;; solid colour
+  (show [0.1 0.7 1.0])
+
+  ;; paterns / checker
+  (show (checker red pink))
+  
+  ;; texture maps
+  (show clojure)
+
+  ;; scaling
+  (show 
+    (scale 0.3
+      (checker red pink)))
+  
+  ;; offset
+  (show 
+    (offset 
+      [0.2 0.2]
+      (checker red pink)))
+  
+  ;; warp
+  (show 
+    (warp
+      (scale 0.2 vnoise)
+      (checker red pink)))
+
+  ;; monochromatic noise
+  (show (scale 0.2 noise))
+  
+  ;; vector noise
+  (show (scale 0.2 vnoise))
+  
+  ;; plasma
+  (show (scale 0.2 plasma))
+  )
